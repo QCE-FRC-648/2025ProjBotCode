6baf8a9ec42c6e57ce9a4b94fd89f940a53c7e23
Theresa L. <33504766+tli228@users.noreply.github.com>
2025-02-03 20:55:22 -0600

    Trying to transfer code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,48 +4,69 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.util.Units;
-import swervelib.math.Matter;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
- * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
- * not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
  *
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants
-{
-
-  public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
-  public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
-  public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
-  public static final double MAX_SPEED  = Units.feetToMeters(14.5);
-  // Maximum speed of the robot in meters per second, used to limit acceleration.
-
-//  public static final class AutonConstants
-//  {
-//
-//    public static final PIDConstants TRANSLATION_PID = new PIDConstants(0.7, 0, 0);
-//    public static final PIDConstants ANGLE_PID       = new PIDConstants(0.4, 0, 0.01);
-//  }
-
-  public static final class DrivebaseConstants
-  {
-
-    // Hold time on motor brakes when disabled
-    public static final double WHEEL_LOCK_TIME = 10; // seconds
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
   }
 
-  public static class OperatorConstants
+  public static class SwerveModuleConstants
   {
+    //may need to change to 12T or 14T
+    public static final double kDrivingMotorPinionTeeth = 14;
 
-    // Joystick Deadband
-    public static final double DEADBAND        = 0.1;
-    public static final double LEFT_Y_DEADBAND = 0.1;
-    public static final double RIGHT_X_DEADBAND = 0.1;
-    public static final double TURN_CONSTANT    = 6;
+    //turn motor is inverted 
+    public static final boolean kTurningEncoderInverted = true;
+
+    public static final double kWheelDiameterMeters = Units.inchesToMeters(2);
+    public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
+
+    //RPM of the NEO brushless motors 
+    public static final double kFreeSpeedRPM = 5676; //rotations per min
+    public static final double kDrivingMotorFreeSpeedRPS = kFreeSpeedRPM / 60; //rotations per sec
+    //gear ratios of driving motor
+    //45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15 teeth on the bevel pinion
+    public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
+    public static final double kDrivingWheelFreeSpeedRPS = (kDrivingMotorFreeSpeedRPS * kWheelCircumferenceMeters) / kDrivingMotorReduction;
+
+    //conversion factors that may be needed
+    public static final double kDrivingEncoderPositionFactor = kWheelCircumferenceMeters / kDrivingMotorReduction; //meters
+    public static final double kDrivingEncoderVelocityFactor =  kDrivingEncoderPositionFactor / 60.0; //meters per second
+
+    public static final double kTurningEncoderPositionFactor = (2 * Math.PI); //radians
+    public static final double kTurningEncoderVelocityFactor = kTurningEncoderPositionFactor / 60.0; //radians per second
+
+    //constants for the PIDs
+    public static final double kDrivingP = 0.006;
+    public static final double kDrivingI = 0;
+    public static final double kDrivingD = 0;
+    public static final double kDrivingFF = 1/kDrivingWheelFreeSpeedRPS;
+    public static final double kDrivingMinOutput = -1;
+    public static final double kDrivingMaxOutput = 1;
+
+    //0.22
+    public static final double kTurningP = 0.4;
+    public static final double kTurningI = 0;
+    public static final double kTurningD = 0;
+    public static final double kTurningMinOutput = -1;
+    public static final double kTurningMaxOutput = 1;
+    public static final boolean kEnablePIDWrapping = true;
+    public static final double kTurningEncoderPositionPIDMinInput = 0;
+    public static final double kTurningEncoderPositionPIDMaxInput = (2 * Math.PI);
+
+    public static final IdleMode kDrivingMotorIdleMode = IdleMode.kBrake;
+    public static final IdleMode kTurningMotorIdleMode = IdleMode.kBrake;
+
+    public static final int kDrivingMotorCurrentLimit = 50; // amps
+    public static final int kTurningMotorCurrentLimit = 20; // amps
   }
 }
